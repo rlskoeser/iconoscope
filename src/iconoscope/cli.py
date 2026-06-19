26380afd9bc9244c5ceedb157f024ebbc4f1543f
rlskoeser <rebecca.s.koeser@princeton.edu>
2026-06-18 22:27:37 -0400

    Add info command to summarize parquet file contents

--- src/iconoscope/cli.py
+++ src/iconoscope/cli.py
@@ -1,6 +1,8 @@
 import argparse
 from pathlib import Path
 
+import polars as pl
+
 from iconoscope.embed import extract_img_features
 from iconoscope.mosaic import generate_mosaic
 
@@ -21,6 +23,24 @@ def main_mosaic(args: argparse.Namespace):
     )
 
 
+def embeddings_info(args: argparse.Namespace):
+    if not args.embeddings.is_file():
+        raise SystemExit(f"{args.embeddings} is not a file")
+    df = pl.read_parquet(args.embeddings)
+
+    info_details = [f"Details for {args.embeddings} :"]
+
+    if "image_path" in df.columns and "features" in df.columns:
+        info_details.append(f"  {df.height:,} images with features extracted")
+    else:
+        info_details.append(" image_path and features columns not found")
+
+    if "umap" in df.columns:
+        info_details.append("  UMAP coordinates")
+
+    print("\n".join(info_details))
+
+
 def main():
     parser = argparse.ArgumentParser(prog="iconoscope")
     subparsers = parser.add_subparsers()
@@ -37,6 +57,14 @@ def main():
         help="File path for saved embeddings (.parquet)",
     )
     parser_embed.set_defaults(func=main_embed)
+
+    info_parser = subparsers.add_parser("info")
+    info_parser.add_argument(
+        "embeddings",
+        type=Path,
+        help="Embeddings file produced by iconoscope embed (.parquet)",
+    )
+    info_parser.set_defaults(func=embeddings_info)
 
     parser_mosaic = subparsers.add_parser("mosaic")
     parser_mosaic.add_argument(
