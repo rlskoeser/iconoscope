8b167ebbfbde3c9ee434e8255d3cc1d84766ae65
rlskoeser <rebecca.s.koeser@princeton.edu>
2026-09-25 15:47:38 -0400

    Simplify message for existing features
    [skip ci]

--- src/iconoscope/commands/embed.py
+++ src/iconoscope/commands/embed.py
@@ -9,9 +9,7 @@ def main(args: argparse.Namespace, model="dinov2") -> None:
     try:
         img_dataset = ImageDataset(storage_path=args.dataset)
         if img_dataset.has_features(model):
-            print(
-                f"Dataset `{args.dataset} already has features for `{model}`; update is not supported."
-            )
+            print(f"Dataset already has features for `{model}`")
             return
 
         start_time = time.perf_counter()
