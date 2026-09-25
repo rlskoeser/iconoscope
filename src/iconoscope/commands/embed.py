fcc8b59043035a3b93e6227c1987419ffc1c3641
rlskoeser <rebecca.s.koeser@princeton.edu>
2026-09-25 15:36:19 -0400

    Don't extract features when already present in the dataset

--- src/iconoscope/commands/embed.py
+++ src/iconoscope/commands/embed.py
@@ -5,15 +5,21 @@ from iconoscope.dataset import ImageDataset
 from iconoscope.embed import extract_img_features
 
 
-def main(args: argparse.Namespace) -> None:
+def main(args: argparse.Namespace, model="dinov2") -> None:
     try:
         img_dataset = ImageDataset(storage_path=args.dataset)
+        if img_dataset.has_features(model):
+            print(
+                f"Dataset `{args.dataset} already has features for `{model}`; update is not supported."
+            )
+            return
+
         start_time = time.perf_counter()
         img_feature_df = extract_img_features(img_dataset)
         duration = time.perf_counter() - start_time
         print(
             f"Successfully extracted features from {img_feature_df.height:,} images in {duration:.2f}s"
         )
-        img_dataset.save_features(img_feature_df, "dinov2")
+        img_dataset.save_features(img_feature_df, model)
     except ValueError as err:
         raise SystemExit(err)
